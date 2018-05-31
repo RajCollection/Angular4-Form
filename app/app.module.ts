8b7fdd16d750a31529ec729a5d2748da1afb4181
Rajpeer <31372812+RajCollection@users.noreply.github.com>
2018-05-31 17:58:33 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+
+
+
+import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateDrivenComponent,
+    ReactiveFormsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
